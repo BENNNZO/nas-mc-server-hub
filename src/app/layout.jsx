74e12896d86221a9c9895e2fb3c3fb8596801d5d
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2026-01-05 17:36:02 -0500

    setup

---
+++ src/app/layout.jsx
@@ -0,0 +1,21 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  weight: ['400', '500', '600', '700']
+})
+
+export const metadata = {
+  title: "Minecraft server list",
+  description: "Manage minecraft docker container servers with ease.",
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased bg-zinc-950 text-white min-h-screen`}>
+        {children}
+      </body>
+    </html>
+  )
+}
