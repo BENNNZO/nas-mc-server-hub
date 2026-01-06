b006a39b3ac45d2e8913b03cf3cfb6bb6385a6ee
BENNNZO <benjamin.phillips.webdev@gmail.com>
2026-01-06 18:10:58 -0500

    Implement: new icons into start/stop button.

--- src/components/server/StartStopButton.jsx
+++ src/components/server/StartStopButton.jsx
@@ -6,8 +6,11 @@ export default function StartStopButton({
   const isStarted = status === 'started'
 
   return (
-    <button onClick={isStarted ? handleStop() : handleStart()} className='bg-zinc-800 hover:bg-zinc-700 px-2 py-1 rounded-md cursor-pointer'>
-      {isStarted ? 'Stop' : 'Start'}
+    <button onClick={isStarted ? handleStop() : handleStart()} className='bg-zinc-800 hover:bg-zinc-700 p-2 rounded-md cursor-pointer'>
+      {isStarted
+        ? <img src="/icons/stop.svg" alt="stop icon" className="size-6 invert opacity-75" />
+        : <img src="/icons/play.svg" alt="play icon" className="size-6 invert opacity-75" />
+      }
     </button>
   )
 }
