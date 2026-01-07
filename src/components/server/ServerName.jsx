0c29886b96a15134ca4225b4c48cb5c051dc9533
BENNNZO <benjamin.phillips.webdev@gmail.com>
2026-01-06 20:44:24 -0500

    Style: restyle serverName.

--- src/components/server/ServerName.jsx
+++ src/components/server/ServerName.jsx
@@ -1,5 +1,5 @@
 export default function ServerName({ name }) {
   return (
-    <h2 className="bg-zinc-800 px-2 py-1 rounded-md border border-zinc-700">{name}</h2>
+    <h2 className="text-lg font-semibold pl-1">{name}</h2>
   )
 }
